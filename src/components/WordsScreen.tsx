bab70ba30406edf83d3d85a564b42e68cd910b0b
ronak-pal1 <ronakpaul882@gmail.com>
2025-07-12 23:12:24 +0530

    Words utils and words screen

--- src/components/WordsScreen.tsx
+++ src/components/WordsScreen.tsx
@@ -1,11 +1,50 @@
-
+import { useEffect, useState } from "react";
+import { getWords } from "../utils/words";
 
 const WordsScreen = () => {
-    return (
-        <div className="w-[1000px] mb-20 text-center">
-            <p className="text-4xl font-VT text-neutral-400">sgag gaea agea aga gaega gaououba gaeoag agea agae hahogea agoueag ageaj ga ga ageag agea gae gage gaea gea gage agea </p>
-        </div>
-    )
-}
+    const [currentKey, setCurrentKey] = useState<string>("")
+  const [str, setStr] = useState<string>("");
+  const [typedStr, setTypedStr] = useState<string>("");
+
+  useEffect(() => {
+    setStr(getWords());
+
+    window.addEventListener("keydown", (e) => {
+        setCurrentKey(e.key)
+    });
+  }, []);
+
+
+
+  useEffect(() => {
+
+    if(!currentKey)
+        return;
+
+      if (str.charAt(0) == currentKey) {
+        setTypedStr(typedStr + currentKey);
+        setStr(str.slice(1));
+      }
+
+
+
+      if(str.length == 0)
+      {
+        setStr(getWords())
+        setTypedStr("")
+      }
+
+
+  }, [currentKey])
+
+  return (
+    <div className="w-[1000px] mb-16 text-center">
+      <p className="text-4xl font-VT text-neutral-400">
+        <span className="text-neutral-700">{typedStr}</span>
+        {str}
+      </p>
+    </div>
+  );
+};
 
 export default WordsScreen;
